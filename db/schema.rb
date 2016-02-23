669c342847d1f2c27b8f12dc934aeaa5f617b3bf
isoltan95 <isoltan@andrew.cmu.edu>
2016-02-23 18:48:25 -0500

    added search functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160223071923) do
+
+  create_table "items", force: :cascade do |t|
+    t.string   "code"
+    t.string   "name"
+    t.string   "category"
+    t.decimal  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end

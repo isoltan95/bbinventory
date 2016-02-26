d589c4b041d1ed9844aa47686e59e8183c4351e5
mogh <mogh@example.com>
2016-02-26 07:01:28 -0800

    check_out action in items controller decreases item quantity by 1 if barcode matches

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -73,7 +73,17 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+#checkout form redirects to this action. If barcode matches, decrease quantity
   def check_out
+    @item = Item.where(code: item_params[:code]).first
+    if @item.nil?
+      redirect_to :checkout, notice: 'Item does not exist in inventory.'
+    elsif @item.quantity == 0
+      redirect_to checkout_url, notice: "Cannot checkout. Current quantity of #{@item.name} is 0."
+    else
+      @item.update_attribute(:quantity, @item.quantity-1)
+      redirect_to items_url, notice: "#{@item.name} was successfully checked out."
+    end
   end
 
   private

f542850bf4b67e465eb564ba6937ad3bc47ae421
mogh <mogh@example.com>
2016-03-16 22:11:42 -0400

    user can specify quantity to add during check in

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root 'items#index'
   
   get 'home' => 'items#index', as: :home
+  get 'checkin' => 'items#checkin', as: :checkin
+  get 'check_in' => 'items#check_in', as: :check_in
   get 'checkout' => 'items#checkout', as: :checkout
   get 'check_out' => 'items#check_out', as: :check_out
 end
